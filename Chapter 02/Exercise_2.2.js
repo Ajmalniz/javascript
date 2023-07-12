c2eedab9d70fb4ca7785a3337e98cf9dac4542f3
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-07-12 08:24:02 +0500

    chapter 2

---
+++ Chapter 02/Exercise_2.2.js
@@ -0,0 +1,5 @@
+const myName = "Ajmal";
+const myAge = 32;
+const coder = true;
+const message = "Hello, my name is " + myName + ", I am " + myAge+" years old and I can code JavaScript: " + coder + "."; 
+console.log(message);
---
+++ Chapter 02/Exercise_2.3.js
@@ -0,0 +1,3 @@
+let a = 3,b=5;
+let hypotenuseVal = ((a * a) + (b * b))**0.5;
+console.log(hypotenuseVal);
